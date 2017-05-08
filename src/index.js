5f5a2f44b46498a62ddedbbf63452159773e96c8
Olesya Kovalenko <olesyaprivat@gmail.com>
2017-05-08 22:39:04 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
  Функция должна принимать один аргумент и возвращать его
  */
 function returnFirstArgument(arg) {
+    return arg;
 }
 
 /*
@@ -15,6 +16,12 @@ function returnFirstArgument(arg) {
  Значение по умолчанию второго аргумента должно быть 100
  */
 function defaultParameterValue(a, b) {
+    if (b) {
+        return a + b;
+    }
+    else {
+        return a + 100;
+    }
 }
 
 /*
@@ -24,6 +31,11 @@ function defaultParameterValue(a, b) {
  Количество переданных аргументов заранее неизвестно
  */
 function returnArgumentsArray() {
+    var arg = [];
+    for (var i = 0; i < arguments.length; i++) {
+        arg[i] = arguments[i];
+    }
+    return arg;
 }
 
 /*
@@ -32,7 +44,9 @@ function returnArgumentsArray() {
  Функция должна принимать другую функцию и возвращать результат вызова переданной функции
  */
 function returnFnResult(fn) {
+    return fn();
 }
+
 
 /*
  Задание 5:
@@ -41,6 +55,10 @@ function returnFnResult(fn) {
  При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
  */
 function returnCounter(number) {
+    var a = number || 0;
+    return function F() {
+        return a + 1;
+    };
 }
 
 /*
@@ -49,7 +67,8 @@ function returnCounter(number) {
  Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
  Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
  */
-function bindFunction(fn) {
+function bindFunction (fn, ...arguments) {
+    return  fn.bind(null, ...arguments);
 }
 
 export {
