b2fffce0abcc9d4f5203bd72a4bfbc577feae57f
Olesya Kovalenko <olesyaprivat@gmail.com>
2017-06-26 22:14:55 +0300

    home9

--- src/index.js
+++ src/index.js
@@ -1,190 +1,352 @@
-function vkApi(method, options) {
-    if (!options.v) {
-        options.v = '5.64';
-    }
+var map;
 
-    return new Promise((resolve, reject) => {
-        VK.api(method, options, data => {
-            if (data.error) {
-                reject(new Error(data.error.error_msg));
-            } else {
-                resolve(data.response);
-            }
-        });
-    });
-}
-
-function vkInit() {
-    return new Promise((resolve, reject) => {
-        VK.init({
-            apiId: 6067884
-        });
-
-        VK.Auth.login(data => {
-            if (data.session) {
-                resolve();
-            } else {
-                reject(new Error('Не удалось авторизоваться'));
-            }
-        }, 2);
-    });
-}
-function addItemList (list, item){
-	list.appendChild(item);
-
-}
-
-function isItemList (list, item){
-	var n=0;
-	for(var i=0; i<list.length; i++){
-		var name = list[i].getAttribute('id');
+var template = `
+<div class="content-info">
+	<div class="header">{{address}}
+	</div>
+	<div class="content-list">
+	<div class="non-review">Отзывов пока нет</div>
+	</div>
+	<div class="form-add">
+		<h3>Ваш отзыв</h3>
 		
-		if (name == item.name){
-			n=1;
-			break;
+		<div>
+			<input type="text" placeholder="Ваше имя"  class="input-text name"/>
+		</div>
+		<div>
+			<input type="text" placeholder="Место"  class="input-text place"/>
+		</div>
+		<div>
+			<textarea placeholder="Ваш отзыв"  class="input-text review"></textarea>
+		</div>
+		<div class="button-container"><button class="button-form">Добавить</button></div>
+
+	</div>
+</div>
+`;
+
+var templateFn = Handlebars.compile(template);
+var markersList = {
+	arrayMarkerAll : [],
+	titleList : [],
+	content : [],
+	contentReview : []
+};
+
+function geocodePlaceId(geocoder, map, latLng) {
+	return new Promise(resolve => {
+		var header = geocoder.geocode({'location': latLng}, function(results, status) {
+		if (status === 'OK') {
+		if (results[1]) {
+			resolve ({address : results[1].formatted_address});	
+		} 
+		else {
+			window.alert('No results found');
 		}
-		
-	}
-	if(n==1){
-		return true;
-	}
-	else{
-		return false;
-	}
+		}
+		else {
+			window.alert('Geocoder failed due to: ' + status);
+		}
+	});
+	})
 }
-function renderFriendsList (list){
+function renderReview (content){
 
-	var friendsList = document.getElementById('friends');
-	var filterList = document.getElementById('friends-filter');
+	var itemRewiew = document.createElement('div');
+	var now = new Date();
+	var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+	var data = now.getFullYear() + "/" + now.getMonth()+ "/" + now.getDate() + ' ' + time;
+	var inputNameValue = content.querySelector('.name').value || "";
+	var inputPlaceValue = content.querySelector('.place').value || "";
+	var inputReviewValue = content.querySelector('.review').value || "";
+	var reviewList = content.querySelector('.content-list');
+	var spanPlace = document.createElement('div');
+	spanPlace.classList.add('place-content');
+	spanPlace.innerHTML = inputPlaceValue;
+	var spanName = document.createElement('span');
+	spanName.classList.add('name-content');
+	spanName.innerHTML = inputNameValue;
+	var divReview = document.createElement('div');
+	divReview.classList.add('review-content');
+	divReview.innerHTML = inputReviewValue;
+	var spanData = document.createElement('span');
+	spanData.innerHTML = data; 
+	itemRewiew.appendChild(spanName);
+	itemRewiew.appendChild(spanPlace);
+	itemRewiew.appendChild(spanData);
+	itemRewiew.appendChild(divReview);
+	reviewList.appendChild(itemRewiew);
+	reviewList.querySelector('.non-review').setAttribute('style', 'display:none');
 	
-	for(var i=0; i<list.length; i++){
-		var li = document.createElement('li');
-		li.setAttribute('id',list[i].last_name);
-		li.setAttribute('draggable', 'true');
-		li.setAttribute('ondragstart', 'dragstart_handler(event)');
-		li.setAttribute('ondragend', 'dragend_handler(event)');
-		li.classList.add("friend-item");
-		var spanImg = document.createElement('span');
-		spanImg.classList.add("photo");
-		var img = document.createElement('img');
-		img.setAttribute('src', list[i].photo_200);
-		spanImg.appendChild(img);
-		var spanName = document.createElement('span');
-		spanName.classList.add("name");
-		spanName.innerText = list[i].first_name + ' ' + list[i].last_name;
-		li.appendChild (spanImg);
-		li.appendChild (spanName);
-		var button = document.createElement('button');
-		li.appendChild (button);
-		friendsList.appendChild (li);
-	};
-
-		
+	var reviewObj = {
+		date: data,
+		name: inputNameValue,
+		place: inputPlaceValue,
+		review: inputReviewValue
+	}
+	markersList.contentReview.push (reviewObj);	
+	clearForm (content);
 }
-function buttonsListener(){
-	var saveButton = document.getElementById('save-button');
-	var filterList = document.getElementById('friends-filter');
-	var friendsList = document.getElementById('friends');
-	var inputFriends = document.getElementById('search-list');
-	var inputFriendsFilter = document.getElementById('search-filter');
-	var conteinerList = document.getElementById('container-list');
-	var container = document.getElementById('content-inner');
+function infoWindowOpen (map, pos, content){
+ 	var infowindow = new google.maps.InfoWindow({
+			position: pos
+	});
+	infowindow.open(map);
+	infowindow.setContent(content);
+	return infowindow;
+}
+function clearForm (content){
+	content.querySelector('.name').value = "";
+	content.querySelector('.place').value = "";
+	content.querySelector('.review').value = "";
+}
+function renderContentPopap (title, map, pos){
+	var content = document.createElement('div');
+	content.innerHTML = templateFn(title);
+	infoWindowOpen (map, pos, content);
+	var arrayMarker = [];
+	
+	content.addEventListener ('click', function(e){
 
-	conteinerList.addEventListener('click', function(e){
-		if(e.target.nodeName == "BUTTON"){
-				var liCurrent =  e.target.parentElement;
-				var list = liCurrent.parentElement;
-				var listId = list.getAttribute('id');
-				clearFilter ();
-				if(listId == 'friends'){
-					addItemList(filterList, liCurrent);
-					
+		if (e.target.nodeName == 'BUTTON'){
+			var marker = new google.maps.Marker({
+				position: pos,
+				map: map
+			});
+
+			marker.addListener('click', function(e){
+				infoWindowOpen (map, pos, content);
+			})
+	
+			markersList.arrayMarkerAll.push(marker);
+			markersList.titleList.push(title);
+			arrayMarker.push(marker);
+			renderReview(content);
+			markersList.content.push(content);
+			var list = content.querySelector('.content-list').children;
+			
+			if(list.length > 1){
+				setCluster(arrayMarker);
+			}
+		}
+	})
+}
+function setCluster(markers) {
+  	var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+	
+	map.addListener('zoom_changed', function () {
+        var markerCluster = new MarkerClusterer(map, markersList.arrayMarkerAll, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+	})	
+
+	ClusterIcon.prototype.triggerClusterClick = function() {
+		var markerClusterer = this.cluster_.getMarkerClusterer();
+		event.stopPropagation();
+		google.maps.event.trigger(markerClusterer, 'clusterclick', this.cluster_);
+		if (markerClusterer.isZoomOnClick(event)) {			
+			var array = this.cluster_.markers_;
+			var clasterObj = getData (array);
+			var contentCarusel = renderCaruselContent (clasterObj, array[0].position);
+			var infowindowCarusel = infoWindowOpen (map, array[0].position, contentCarusel);
+			var addressList  = document.querySelectorAll('.carusel_wrap .adress');
+			for (var i=0; i<addressList.length; i++){
+				addressList[i].addEventListener('click', function(e){
+				var title = e.target.innerHTML;
+				var number = isTtitle (clasterObj.titles, title);
+				infowindowCarusel.close();
+				infoWindowOpen(map, array[0].position, clasterObj.content[number]);
+			})
+  }
+
+		}
+	};
+}
+function isTtitle (array, title){
+	var n = -1;
+	for (var i=0; i<array.length; i++){
+		if (array[i] == title){
+			n=i;
+		}
+	}
+	return n;
+}
+
+function getData (clasterMarker){
+	var allMarkers = markersList.arrayMarkerAll;
+	var titleListArray = markersList.titleList;
+	var listContent = markersList.content;
+	var clasterObj = {
+		titles : [],
+		markers : [],
+		content : [],
+		contentReview : []
+	}
+	var n = [];
+	for(var j=0; j<clasterMarker.length; j++){
+		
+		for (var i=0; i<allMarkers.length; i++){
+			if (allMarkers [i].position.lat() == clasterMarker[j].position.lat() && allMarkers [i].position.lng() == clasterMarker[j].position.lng()){
+			
+				if(n.length==0){
+					n.push(i);	
 				}
-				else {
-					addItemList(friendsList, liCurrent);
+				else{
+					if(isTtitle (n, i)==-1){
+					n.push(i);	
+				}
 				}
 			}
-	});
-		saveButton.addEventListener('click', function(){
-			clearFilter ();
-			var content = container.innerHTML;
-			localStorage.setItem('content', JSON.stringify(content));	
-		});
-}
-function renderFilter(){
-	var inputFriends = document.getElementById('search-list');
-	var inputFriendsFilter = document.getElementById('search-filter');
-	var filterList = document.getElementById('friends-filter');
-	var friendsList = document.getElementById('friends');
-	inputFriends.addEventListener('keyup', function(){
-		var inputValue = inputFriends.value;
-		filter(friendsList, inputValue);
-		});	
-	inputFriendsFilter.addEventListener('keyup', function(){
-		var inputValue = inputFriendsFilter.value;
-		filter(filterList, inputValue);
-	});	
-}
-function clearFilter (){
-	var liList = document.querySelectorAll('li');
-	var inputFriends = document.getElementById('search-list');
-	inputFriends.value="";
-	var inputFriendsFilter = document.getElementById('search-filter');
-	inputFriendsFilter.value="";
-	for (var i=0; i<liList.length; i++){
-		liList[i].classList.remove('display-none');
-	}
-	
-}
-function isMatching(item, value) {
+		}
 
-	var str = item.toUpperCase();
-	var subStr = value.toUpperCase();
-	if (str.indexOf(subStr)<0){
-		return false;
 	}
-	else{
-		return true;
+	for (var i=0; i<n.length; i++){
+		var number = n[i];
+		clasterObj.titles.push (titleListArray[number].address);
+		clasterObj.markers.push (allMarkers[number]);
+		clasterObj.content.push (listContent[number]);
+		clasterObj.contentReview.push (markersList.contentReview[number]);
 	}
+	return clasterObj;
 }
+function renderCaruselContent (clasterObj, position){
 
-function filter(list, value){
-	var nameList = list.querySelectorAll('.name');
-	for  (var i=0; i<nameList.length; i++){
-			if (!isMatching(nameList[i].innerText,  value)){
-				nameList[i].parentElement.classList.remove('display-block');
-				nameList[i].parentElement.classList.add('display-none');
+	var content = document.createElement('div');
+	content.classList.add('carusel_wrap');
+	var ulList = document.createElement('ul');
+  	ulList.setAttribute('id', 'list');
+	ulList.setAttribute('class', 'list');
+  	var links  = document.createElement('ul');
+  	links.setAttribute('id', 'links');
+	links.setAttribute('class', 'links');
+	var wrapper = document.createElement('div');
+	wrapper.setAttribute('id', 'carousel');
+	wrapper.setAttribute('class', 'carousel');
+	var j = 0;
+
+	for (var i = 0; i < clasterObj.titles.length; i++) {
+		var li = document.createElement('li');
+		var divPlace = document.createElement('div');
+		divPlace.classList.add('place');
+		var divAdress = document.createElement('div');
+		divAdress.classList.add('adress');
+		var divTextReview = document.createElement('div');
+		divTextReview.classList.add('text-review');
+		var divDataReview = document.createElement('div');
+		divDataReview.classList.add('data-review');
+		divPlace.innerHTML = clasterObj.contentReview[i].place;
+		divAdress.innerHTML = clasterObj.titles[i];
+		divTextReview.innerHTML = clasterObj.contentReview[i].review;
+		divDataReview.innerHTML = clasterObj.contentReview[i].date;
+ 		li.appendChild(divPlace);
+		li.appendChild(divAdress);
+		li.appendChild(divTextReview);
+		li.appendChild(divDataReview);
+		ulList.appendChild(li);
+
+		var linkLi = document.createElement('li');
+		linkLi.setAttribute('id', i);
+		linkLi.innerHTML = ++j;
+		links.appendChild(linkLi);
+		if (i == 0){
+			li.classList.add('display-block');
+			linkLi.classList.add('active');
+		}
+	}
+  	content.appendChild (ulList);
+  	content.appendChild (links);
+  	var listitems = ulList.children;
+	var linksitems = links.children;
+ 	links.addEventListener('click', function(e){
+		var id = e.target.getAttribute('id');
+		for (var i = 0; i < listitems.length; i++){
+			listitems[i].classList.remove('display-block');
+			if (i==id){
+				listitems[i].classList.add('display-block');
 			}
 			else{
-				nameList[i].parentElement.classList.remove('display-none');
-				nameList[i].parentElement.classList.add('display-block');
+				listitems[i].classList.remove('display-block');
+			}
+    	 }	
+
+		for (var i = 0; i < linksitems.length; i++){
+			linksitems[i].classList.remove('active');
+		}
+		e.target.classList.add('active');
+  	}); 
+  	var buttonPrev=document.createElement('button');
+  	buttonPrev.setAttribute('id', 'prev');
+	buttonPrev.innerText = '<' 
+ 	var buttonNext=document.createElement('button');
+  	buttonNext.setAttribute('id', 'next');
+	buttonNext.innerText = '>' 
+  	content.appendChild (buttonPrev);
+  	content.appendChild (buttonNext);
+  	buttonPrev.addEventListener('click', function(){
+		for (var i=0; i<listitems.length; i++){
+			var classItem = listitems[i].classList;
+			if(isTtitle(classItem, 'display-block')>=0){
+				if(listitems[i].previousSibling){
+					listitems[i].classList.remove('display-block');
+					listitems[i].previousSibling.classList.add('display-block');
+
+					for (var j = 0; j < linksitems.length; j++){
+						linksitems[j].classList.remove('active');
+					}
+					linksitems[i-1].classList.add('active');
+					break;
+				}
 			}
 		}
+  	});
+ 	buttonNext.addEventListener('click', function(){
+     	for (var i=0; i<listitems.length; i++){
+			var classItem = listitems[i].classList;
+			if(isTtitle(classItem, 'display-block')>=0){
+				if(listitems[i].nextSibling){
+					listitems[i].classList.remove('display-block');
+					listitems[i].nextSibling.classList.add('display-block');
+					for (var j = 0; j < linksitems.length; j++){
+						linksitems[j].classList.remove('active');
+					}
+					linksitems[i+1].classList.add('active');
+					break;
+				}
+			}
+		}
+  	});
+  	return content;
 }
 
-
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+     
+}
+  
 new Promise(resolve => window.onload = resolve)
-    .then(() => {
-		vkInit();
+.then(() => {
+	initMap();
+	var mc = new MarkerClusterer(map);
+})
+.then(() => {
+	map.addListener('click', function(e , event) {
+		var title;
+		var geocoder = new google.maps.Geocoder;
+		geocodePlaceId(geocoder, map, e.latLng).then(response =>{
+		title = response;
 	})
+	.then(()=>{
+		renderContentPopap (title, map , e.latLng);
+	})
+	});
+	var a = markersList.arrayMarkerAll;
+	setCluster(markersList.arrayMarkerAll);
+})
 
-    .then(() => vkApi('friends.get', {fields: 'photo_200'}))
-    .then(response => {
-		
-		var listFriend = response.items;
-		var container = document.getElementById('content-inner');
-		
-		if (localStorage.getItem('content')){
-			var contentInner = localStorage.getItem('content');
-			var contentText = JSON.parse(contentInner);
-			container.innerHTML = contentText;
-			renderFilter();
-			buttonsListener();
-		}
-		else{
-			renderFriendsList(listFriend);
-			renderFilter();
-			buttonsListener()
-		}
-	})
-    .catch(e => alert('Ошибка: ' + e.message));
+	  
+	  
+	  
+	  
+	  
